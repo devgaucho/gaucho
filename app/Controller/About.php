4158b735bff335c077077f4571211f3bcc8a154a
devgaucho <119246170+devgaucho@users.noreply.github.com>
2024-02-06 20:02:58 -0300

    M app/Controller/About.php

--- app/Controller/About.php
+++ app/Controller/About.php
@@ -1,15 +1,18 @@
-<?php 
+<?php
 namespace App\Controller;
 
 use Gaucho\Gaucho;
 
-class About extends Gaucho{
-	function GET(){
-		$data=[
-			'title'=>'Sobre'
-		];
-        $this->chaplin('inc/header',$data);
-		$this->chaplin('about',$data);
-		$this->chaplin('inc/footer',$data);
-	}
+class About extends Gaucho
+{
+
+    function GET()
+    {
+        $data = [
+            'title' => 'Sobre'
+        ];
+        $this->chaplin('inc/header', $data);
+        $this->chaplin('about', $data);
+        $this->chaplin('inc/footer', $data);
+    }
 }
