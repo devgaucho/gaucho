ec4e2474c57fc42cbffcb23dec2e4c61a71d72a5
devgaucho <119246170+devgaucho@users.noreply.github.com>
2024-02-08 18:56:26 -0300

    zen

--- cfg.php
+++ cfg.php
@@ -1,12 +1,9 @@
 <?php
-
-const ROOT = __DIR__;
-require ROOT . '/vendor/autoload.php';
-
+const ROOT=__DIR__;
+require ROOT.'/vendor/autoload.php';
 use Gaucho\Gaucho;
-
-$Gaucho = new Gaucho();
-if (! $Gaucho->isCli() and file_exists(ROOT . '/off')) {
+$Gaucho=new Gaucho();
+if(!$Gaucho->isCli() and file_exists(ROOT.'/off')){
     http_response_code(503);
     die("maintenance mode");
 }
